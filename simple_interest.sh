82adccb632a50a980bbca0c6f506bf36394879e6
jinesh puglia <72568435+jineshpugliya@users.noreply.github.com>
2023-09-25 18:08:31 +0530

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -5,8 +5,8 @@
    # Do not use this in production. Sample purpose only.
 
    # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+   # Additional Authors: Jinesh Puglia
+   # <jineshpugliya>
 
    # Input:
    # p, principal amount
